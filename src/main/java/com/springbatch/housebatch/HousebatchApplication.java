baf3309547106c327847d5ab5742ce0f5a6d5bce
derulo taser <iih907@naver.com>
2024-05-05 14:04:22 +0900

    Initial commit

---
+++ src/main/java/com/springbatch/housebatch/HousebatchApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.housebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousebatchApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springbatch/housebatch/core/repository/LawdRepository.java
@@ -0,0 +1,7 @@
+package com.springbatch.housebatch.core.repository;
+
+import com.springbatch.housebatch.core.entity.Lawd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawdRepository extends JpaRepository <Lawd, Long> {
+}
